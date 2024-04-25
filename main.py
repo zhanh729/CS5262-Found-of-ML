9569c43d16e1dec02f6bcdc356724de8428a0c33
burnsnj2 <nicholas.j.burns@vanderbilt.edu>
2024-04-24 21:36:43 -0500

    Fixed positions and added printing of predicted All-Stars

--- main.py
+++ main.py
@@ -29,6 +29,14 @@ df_train.at[550, 'Player'] = 'Luka Samanic'
 df_train.at[551, 'Player'] = 'Dario Saric'
 df_train.at[614, 'Player'] = 'Jonas Valanciunas'
 df_train.at[622, 'Player'] = 'Nikola Vucevic'
+# some players have multiple positions
+# print(df_train[df_train['Pos'].str.contains('-', regex=True)])
+# correct their positions manually
+df_train.at[174, 'Pos'] = 'PF'
+df_train.at[254, 'Pos'] = 'SF'
+df_train.at[346, 'Pos'] = 'PF'
+df_train.at[463, 'Pos'] = 'SG'
+df_train.at[631, 'Pos'] = 'SF'
 
 # read testing data from csv
 df_test_A = pd.read_csv('2021-2022 NBA Player Stats - Regular.csv', encoding='ISO-8859-1', sep=";")
@@ -50,6 +58,27 @@ df_test_A.at[601, 'Player'] = 'Kristaps Porzingis'
 df_test_A.at[651, 'Player'] = 'Tomas Satoransky'
 df_test_A.at[743, 'Player'] = 'Jonas Valanciunas'
 df_test_A.at[751, 'Player'] = 'Nikola Vucevic'
+# some players have multiple positions
+# print(df_test_A[df_test_A['Pos'].str.contains('-', regex=True)])
+# correct their positions manually
+df_test_A.at[81, 'Pos'] = 'SG'
+df_test_A.at[89, 'Pos'] = 'SG'
+df_test_A.at[146, 'Pos'] = 'PF'
+df_test_A.at[276, 'Pos'] = 'PG'
+df_test_A.at[282, 'Pos'] = 'PG'
+df_test_A.at[294, 'Pos'] = 'SF'
+df_test_A.at[315, 'Pos'] = 'SG'
+df_test_A.at[325, 'Pos'] = 'SG'
+df_test_A.at[329, 'Pos'] = 'SF'
+df_test_A.at[343, 'Pos'] = 'SG'
+df_test_A.at[350, 'Pos'] = 'C'
+df_test_A.at[435, 'Pos'] = 'SG'
+df_test_A.at[458, 'Pos'] = 'SG'
+df_test_A.at[487, 'Pos'] = 'PG'
+df_test_A.at[607, 'Pos'] = 'SG'
+df_test_A.at[647, 'Pos'] = 'C'
+df_test_A.at[657, 'Pos'] = 'SG'
+df_test_A.at[698, 'Pos'] = 'SG'
 
 # read testing data from csv
 df_test_B = pd.read_csv('2023-2024 NBA Player Stats - Regular.csv', encoding='ISO-8859-1', sep=";")
@@ -72,6 +101,22 @@ df_test_B.at[549, 'Player'] = 'Luka Samanic'
 df_test_B.at[553, 'Player'] = 'Dario Saric'
 df_test_B.at[628, 'Player'] = 'Jonas Valanciunas'
 df_test_B.at[634, 'Player'] = 'Nikola Vucevic'
+# some players have multiple positions
+# print(df_test_B[df_test_B['Pos'].str.contains('-', regex=True)])
+# correct their positions manually
+df_test_B.at[0, 'Pos'] = 'PF'
+df_test_B.at[70, 'Pos'] = 'SF'
+df_test_B.at[91, 'Pos'] = 'SG'
+df_test_B.at[136, 'Pos'] = 'SF'
+df_test_B.at[203, 'Pos'] = 'C'
+df_test_B.at[206, 'Pos'] = 'PF'
+df_test_B.at[270, 'Pos'] = 'SF'
+df_test_B.at[279, 'Pos'] = 'C'
+df_test_B.at[396, 'Pos'] = 'PG'
+df_test_B.at[518, 'Pos'] = 'PG'
+df_test_B.at[542, 'Pos'] = 'PF'
+df_test_B.at[587, 'Pos'] = 'SG'
+df_test_B.at[613, 'Pos'] = 'C'
 
 def regress_sets_of_n(df, stats, test_list, n):
     sets = list(combinations(stats, n))
@@ -79,21 +124,25 @@ def regress_sets_of_n(df, stats, test_list, n):
     max_recall = 0
     max_set = []
     max_recall_set = []
+    pred = []
     for set in sets:
-        score, recall = run_logistic_regression(df, list(set), test_list)
-        if score > max_score:
-            max_score = score
-            max_set = set
+        score, recall, pred_allstars_list = run_logistic_regression(df, list(set), test_list)
         if recall > max_recall:
             max_recall = recall
             max_recall_set = set
-    print("The most accurate set is " + str(max_set) + " with an accuracy of " + str(max_score))
-    print("The set with the best recall is " + str(max_recall_set) + " with a recall of " + str(max_recall))
+            max_score = score
+            pred = pred_allstars_list
+    print("The best set of stats is " + str(max_recall_set))
+    print("Accuracy: " + str(max_score))
+    print("Recall: " + str(max_recall))
+    for i in range(len(pred)):
+        print("Predicted All-Stars (Testing Set " + str(i + 1) + "):")
+        print(', '.join(pred[i]))
 
 # function to run logistic regression
 def run_logistic_regression(df, stats, test_list):
     # scales the data
-    df_reduced = reduce_players(df, 1)
+    df_reduced = reduce_players(df, 2)
     df_scaled = scale(df_reduced)
 
     # creates features and target arrays
@@ -111,6 +160,7 @@ def run_logistic_regression(df, stats, test_list):
 
     test_scores = []
     recalls = []
+    pred_allstars_list = []
     for i in range(len(test_list)):
         df_test_scaled = scale(test_list[i])
         X_test = df_test_scaled[stats].to_numpy()
@@ -123,10 +173,14 @@ def run_logistic_regression(df, stats, test_list):
         recall = recall_score(y_test, y_test_pred)
         recalls.append(recall)
 
+        mask = y_test_pred.astype(bool)
+        pred_allstars = test_list[i][mask]['Player']
+        pred_allstars_list.append(pred_allstars)
+
     avg_test_score = sum(test_scores) / len(test_scores)
     avg_recall = sum(recalls) / len(recalls)
     
-    return avg_test_score, avg_recall
+    return avg_test_score, avg_recall, pred_allstars_list
 
 # function to standardize data
 def scale(df):
